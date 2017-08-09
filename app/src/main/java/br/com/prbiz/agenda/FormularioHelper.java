5a9aa573e0069271d9eccc1c253e694214f52f65
prbizarro <prbizarro@hotmail.com>
2017-08-09 20:01:21 -0300

    Agenda 1 completa beta

---
+++ app/src/main/java/br/com/prbiz/agenda/FormularioHelper.java
@@ -0,0 +1,48 @@
+package br.com.prbiz.agenda;
+
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import br.com.prbiz.agenda.modelo.Aluno;
+
+/**
+ * Created by prbiz on 09/08/2017.
+ */
+
+public class FormularioHelper{
+
+    private final EditText campoNome;
+    private final EditText campoEndereco;
+    private final EditText campoSite;
+    private final EditText campoTelefone;
+    private final RatingBar campoNota;
+
+    private Aluno aluno;
+
+    public FormularioHelper(FormularioActivity activity){
+        campoNome = (EditText) activity.findViewById(R.id.formulario_nome);
+        campoEndereco = (EditText) activity.findViewById(R.id.formulario_endereco);
+        campoSite = (EditText) activity.findViewById(R.id.formulario_site);
+        campoTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
+        campoNota = (RatingBar) activity.findViewById(R.id.formulario_nota);
+        aluno = new Aluno();
+    }
+
+    public Aluno getAluno() {
+        aluno.setNome(campoNome.getText().toString());
+        aluno.setEndereco(campoEndereco.getText().toString());
+        aluno.setSite(campoSite.getText().toString());
+        aluno.setTelefone(campoTelefone.getText().toString());
+        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+        return aluno;
+    }
+
+    public void preencheFormulario(Aluno aluno) {
+        campoNome.setText(aluno.getNome());
+        campoEndereco.setText(aluno.getEndereco());
+        campoTelefone.setText(aluno.getTelefone());
+        campoSite.setText(aluno.getSite());
+        campoNota.setProgress(aluno.getNota().intValue());
+        this.aluno = aluno;
+    }
+}
